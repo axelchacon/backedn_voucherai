4c10c1d3fa65354ca6b1638e08afbcd2d46375e4
Axel Diego Chacón Pérez <axel.chacon@pucp.edu.pe>
2025-01-18 22:30:09 -0500

    okok, casi conectado

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { VouchersModule } from './vouchers/vouchers.module';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [VouchersModule, ConfigModule.forRoot()],
   controllers: [],
-  providers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
